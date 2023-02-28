8ffaef5e7d67c14e34756873a27bbdf492321bf1
Marcos Paulo Alves Rodrigues <marcospauloalvesrodrigues@MacBook-Air-de-Marcos.local>
2023-02-27 21:32:11 -0400

    feat: add scenario login success

--- tests/login.test.js
+++ tests/login.test.js
@@ -12,13 +12,42 @@ test('User cannot login with invalid credentials', async t => {
 	await t.expect(loginForm.exists).ok()
 
 	const usernameInput = Selector('#user_login')
-	const passwordImput = Selector('#user_password')
+	const passwordInput = Selector('#user_password')
 	await t.typeText(usernameInput, 'invalid username', { paste: true })
-	await t.typeText(passwordImput, 'invalid password', { paste: true })
+	await t.typeText(passwordInput, 'invalid password', { paste: true })
 
 	const submitButton = Selector('.btn-primary')
 	await t.click(submitButton)
 
 	const errorMessage = Selector('.alert-error').innerText
 	await t.expect(errorMessage).contains('Login and/or password are wrong.')
+})
+
+test('User can login into application ', async t => {
+	const signInButton = Selector('#signin_button')
+	await t.click(signInButton)
+
+	const loginForm = Selector('#login_form')
+	await t.expect(loginForm.exists).ok()
+
+	const usernameInput = Selector('#user_login')
+	const passwordInput = Selector('#user_password')
+	await t.typeText(usernameInput, 'username', { paste: true })
+	await t.typeText(passwordInput, 'password', { paste: true })
+
+	const submitButton = Selector('.btn-primary')
+	await t.click(submitButton)
+
+	const accountSummaryTab = Selector('#account_summary_tab')
+	await t.expect(accountSummaryTab.exists).ok()
+	await t.expect(loginForm.exists).notOk()
+
+	const userIcon = Selector('.icon-user')
+	await t.click(userIcon)
+
+	const logoutButton = Selector('#logout_link')
+	await t.click(logoutButton)
+
+	await t.expect(logoutButton.exists).notOk()
+	await t.expect(signInButton.exists).ok()
 })
